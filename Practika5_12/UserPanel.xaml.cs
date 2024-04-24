8e15d8aa667a454e692bd8a4a23ca4b7dcd2b579
EduardWer <eduard.unanov66@mail.ru>
2024-04-24 12:33:24 +0000

    Добавьте файлы проекта.

---
+++ Practika5_12/UserPanel.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practika5_12
+{
+    /// <summary>
+    /// Логика взаимодействия для UserPanel.xaml
+    /// </summary>
+    public partial class UserPanel : Page
+    {
+        public UserPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
